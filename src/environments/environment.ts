54804e1ff866fc9c7969c1d9e844a2dd114fa161
Ernesto Bonet Moncada <bernestoalberto@gmail.com>
2020-04-05 18:55:19 -0400

    adding  ng8 components

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,18 @@
 export const environment = {
   production: false,
   firebase:  {
-    apiKey: 'AIzaSyAKlEX5a8PQaadYNSrohqfl33O37Kjkv1Y',
-    authDomain: 'angular-voxer.firebaseapp.com',
-    databaseURL: 'https://angular-voxer.firebaseio.com',
-    projectId: 'angular-voxer',
-    storageBucket: 'angular-voxer.appspot.com',
-    messagingSenderId: '122050526979',
-    appId: '1:122050526979:web:529304dce5ffca7a'
+    apiKey: 'AIzaSyDoqRhvIvb4t6Tl6z9svVnOMG1hWKsD3TU',
+    authDomain: 'adminmoftbot.firebaseapp.com',
+    tokenUrl: 'https://securetoken.googleapis.com/v1/token?key=',
+    BASE_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:',
+    databaseURL: 'https://adminmoftbot.firebaseio.com',
+    projectId: 'adminmoftbot',
+    storageBucket: 'adminmoftbot.appspot.com',
+    messagingSenderId: '867081262900',
+    gcm_sender_id: '103953800507',
+    web_push_certf: 'BLy27zooLeTKSZpvzwVOh2GV_riqZlWfX54JvuDC0yCLEHIR-S4aWOz4tTvahqiNfk1djMHy_SQcC3ynYwbxB9E',
+    appId: '1:867081262900:web:519f5d712e703bb95eb761',
+    measurementId: 'G-FV1NWX6479'
   }
 };
 
